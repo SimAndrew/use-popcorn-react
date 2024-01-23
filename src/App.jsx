6bd0127d9cd159bb08101131eaa2898581d528b8
AndSim <siminiucandrew@gmail.com>
2024-01-23 14:59:53 +0200

    fix: clean up fetch request (Search movie create multiple request at the same time)

--- src/App.jsx
+++ src/App.jsx
@@ -32,12 +32,15 @@ export default function App() {
 
 	useEffect(
 		function () {
+			const controller = new AbortController();
+
 			async function fetchMovies() {
 				try {
 					setIsLoading(true);
 					setError('');
 					const res = await fetch(
 						`http://www.omdbapi.com/?apikey=${KEY}&s=${query}`,
+						{ signal: controller.signal },
 					);
 					if (!res.ok)
 						throw new Error('Something went wrong with Fetching movies!');
@@ -46,8 +49,11 @@ export default function App() {
 					if (data.Response === 'False') throw new Error('Movie not found');
 
 					setMovies(data.Search);
+					setError('');
 				} catch (err) {
-					setError(err.message);
+					if (err.name !== 'AbortError') {
+						setError(err.message);
+					}
 				} finally {
 					setIsLoading(false);
 				}
@@ -60,6 +66,10 @@ export default function App() {
 			}
 
 			fetchMovies();
+
+			return function () {
+				controller.abort();
+			};
 		},
 		[query],
 	);
