fd34adce7d92268b3df7bd389a6775ba8c86023a
AndSim <siminiucandrew@gmail.com>
2024-01-26 20:11:21 +0200

    refactor: extract useEffect function in custom Hook(useMovies.jsx)

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
-import { useEffect, useRef, useState } from 'react';
+import { useCallback, useEffect, useRef, useState } from 'react';
 import StarRating from './StarRating.jsx';
+import { useMovies } from './useMovies.jsx';
 
 const average = (arr) =>
 	arr.reduce((acc, cur, i, arr) => acc + cur / arr.length, 0);
@@ -8,12 +9,14 @@ const KEY = 'cb046bd3';
 
 export default function App() {
 	const [query, setQuery] = useState('');
-	const [movies, setMovies] = useState([]);
-	const [isLoading, setIsLoading] = useState(false);
-	const [error, setError] = useState('');
 	const [selectedId, setSelectedId] = useState(null);
 
-	// const [watched, setWatched] = useState([]);
+	const handleCloseMovie = useCallback(() => {
+		setSelectedId(null);
+	}, []);
+
+	const { movies, isLoading, error } = useMovies(query, handleCloseMovie);
+
 	const [watched, setWatched] = useState(function () {
 		const storedValue = localStorage.getItem('watched');
 		return JSON.parse(storedValue);
@@ -21,10 +24,6 @@ export default function App() {
 
 	function handleSelectMovie(id) {
 		setSelectedId((selectedId) => (id === selectedId ? null : id));
-	}
-
-	function handleCloseMovie() {
-		setSelectedId(null);
 	}
 
 	function handleAddWatched(movie) {
@@ -40,51 +39,6 @@ export default function App() {
 			localStorage.setItem('watched', JSON.stringify(watched));
 		},
 		[watched],
-	);
-
-	useEffect(
-		function () {
-			const controller = new AbortController();
-
-			async function fetchMovies() {
-				try {
-					setIsLoading(true);
-					setError('');
-					const res = await fetch(
-						`http://www.omdbapi.com/?apikey=${KEY}&s=${query}`,
-						{ signal: controller.signal },
-					);
-					if (!res.ok)
-						throw new Error('Something went wrong with Fetching movies!');
-
-					const data = await res.json();
-					if (data.Response === 'False') throw new Error('Movie not found');
-
-					setMovies(data.Search);
-					setError('');
-				} catch (err) {
-					if (err.name !== 'AbortError') {
-						setError(err.message);
-					}
-				} finally {
-					setIsLoading(false);
-				}
-			}
-
-			if (query.length < 3) {
-				setMovies([]);
-				setError('');
-				return;
-			}
-
-			handleCloseMovie();
-			fetchMovies();
-
-			return function () {
-				controller.abort();
-			};
-		},
-		[query],
 	);
 
 	return (
